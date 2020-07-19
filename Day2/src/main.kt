87aa61c22e01097d4b5f16d67a8077599ceb8611
wanitchar <68175782+wanitchar@users.noreply.github.com>
2020-07-19 12:01:18 +0700

    new loop

--- Day2/src/main.kt
+++ Day2/src/main.kt
@@ -1,3 +1,9 @@
-fun main(){
-    println() ("This is Day2!")
+fun main() {
+    var x = 1
+    println("Before the loop. x=$x.")
+    while (x < 4) {
+        println("In the loop. x=$x.")
+        x = x + 1
+    }
+    println("After the loop.x=$x.")
 }
